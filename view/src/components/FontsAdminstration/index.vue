fada8b7e463827f5c3fdf2d8382b85b113da7501
TuiMao233 <wwu710632@gmail.com>
2021-07-03 00:17:34 +0800

    🎨 优化 UI 界面。

--- view/src/components/FontsAdminstration/index.vue
+++ view/src/components/FontsAdminstration/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-06-30 20:33:06
- * @LastEditTime: 2021-07-02 20:50:34
+ * @LastEditTime: 2021-07-03 00:05:22
  * @Description: 
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -29,7 +29,7 @@
       </n-layout-sider>
       <n-layout-content
         class="ml-24"
-        content-style="display: flex; flex-direction: column;"
+        content-style="display: flex; flex-direction: column;height:100%"
         :native-scrollbar="false"
       >
         <n-upload
--- view/vite.config.ts
+++ view/vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-05-17 18:48:39
- * @LastEditTime: 2021-07-02 22:41:22
+ * @LastEditTime: 2021-07-03 00:13:54
  * @Description: vite.config.ts
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -29,6 +29,7 @@ export default defineConfig({
       '~': resolve(__dirname, 'src/components')
     }
   },
+  base: '/fonts',
   build: {
     outDir: '../package/public'
   },
