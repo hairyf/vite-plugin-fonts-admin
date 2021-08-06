e6096c6f30b59dab80ea357c6bdd835fb467a850
燃芯 <56920162+RanxinStart@users.noreply.github.com>
2021-08-06 14:08:23 +0800

    修复无法显示矩形

--- plugin/middlewares.ts
+++ plugin/middlewares.ts
@@ -5,7 +5,7 @@ import path from 'path'
 import svgtofont from 'svgtofont'
 import archiver from 'archiver'
 import { generateSvgCahes, archiverLogger } from './utils'
-import { setHtmlStrTagAttr } from '@tuimao/utils/package/common'
+import { setHtmlStrTagAttr } from '@tuimao/core'
 import multer from 'multer'
 import { nanoid } from 'nanoid'
 const utils = require('nodejs-fs-utils')
@@ -169,14 +169,7 @@ export const fontAdminMiddlewares = (option: FontsPluginOption = {}) => {
       }))
       .map((v) => {
         if (!isRetainColor) {
-          v.value = setHtmlStrTagAttr({
-            html: v.value, tag: 'path',
-            attr: 'fill', value: 'currentColor'
-          })
-          v.value = setHtmlStrTagAttr({
-            html: v.value, tag: 'svg',
-            attr: 'fill', value: 'currentColor'
-          })
+          v.value = setHtmlStrTagAttr(v.value,{ tag: ['path','svg'],attr: 'fill', value: 'currentColor'})
         }
         return v
       })
